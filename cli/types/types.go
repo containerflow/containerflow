215257725a44d97d6915f2ceda2259aeea5d78b1
yunl.zheng <yunl.zheng@gmail.com>
2017-12-26 17:53:18 +0800

    create service link

--- cli/types/types.go
+++ cli/types/types.go
@@ -56,33 +56,91 @@ type Pipeline struct {
 }
 
 // Execute Start pipeline process
-func (pipeline Pipeline) Execute(root string, number int) {
-	fmt.Println("## Set Environment")
+func (pipeline Pipeline) Execute(root string, number int) (err error) {
+	fmt.Println("## Set Build Environment")
+	services := make(map[string]string)
 
-	for _, stage := range pipeline.Spec.Stages {
-		fmt.Println("## Stage:" + stage.Name + " Process")
-		stage.execute(pipeline.Metadata.Namespace, pipeline.Metadata.Name, root)
-		fmt.Println("--> Success")
+	for _, service := range pipeline.Spec.Services {
+		fmt.Println("--> Start service " + service.Name)
+		cid, err := service.start(pipeline.Metadata.Namespace, pipeline.Metadata.Name)
+		if err == nil {
+			services[service.Name] = cid
+		} else {
+			log.Fatalln(err)
+		}
 	}
+
+	fmt.Println("## Start Build")
+	for _, stage := range pipeline.Spec.Stages {
+		fmt.Println("--> Stage:" + stage.Name + " Process")
+		stage.execute(pipeline.Metadata.Namespace, pipeline.Metadata.Name, root, services)
+	}
+
+	fmt.Println("## CleanUp Build Environment")
+
+	for name, cid := range services {
+		fmt.Println("--> Stop Service " + name)
+		forceRemoveContainer(cid)
+	}
+
+	return
 }
 
-func (stage Stage) execute(namespace, name, root string) {
+func (stage Stage) execute(namespace, name, root string, links map[string]string) {
 	var size = len(stage.Tasks)
 	stage.cnum = make(chan int, size)
 	for _, task := range stage.Tasks {
-		go task.execute(namespace, name, stage.Name, root, stage.Workspace, stage.cnum)
+		go task.execute(namespace, name, stage.Name, root, stage.Workspace, stage.cnum, links)
 	}
 	for i := 0; i < size; i++ {
 		<-stage.cnum
 	}
 }
 
-func (task Task) execute(namespace string, name string, stage string, root string, workspace string, cnum chan int) {
-	executeContainer(namespace, name, stage, root, workspace, task)
+func (task Task) execute(namespace string, name string, stage string, root string, workspace string, cnum chan int, links map[string]string) {
+	executeContainer(namespace, name, stage, root, workspace, task, links)
 	cnum <- 1
 }
 
-func executeContainer(namespace string, name string, stage string, root string, workspace string, task Task) bool {
+func (service Service) start(namespace, name string) (cid string, err error) {
+	containerName := fmt.Sprintf("%s-%s-%s", namespace, name, service.Name)
+	containerName = strings.Replace(containerName, " ", "_", -1)
+
+	cli, err := client.NewEnvClient()
+	if err != nil {
+		return
+	}
+
+	_, err = cli.ImagePull(
+		context.Background(),
+		service.Box,
+		types.ImagePullOptions{
+			All: true,
+		},
+	)
+
+	if err != nil {
+		return
+	}
+
+	cid, err = startContainer(&container.Config{
+		Tty:          true,
+		Image:        service.Box,
+		AttachStdout: true,
+		AttachStderr: true,
+		Env:          service.Environment,
+	}, &container.HostConfig{
+		AutoRemove: false,
+	}, containerName)
+
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+func executeContainer(namespace string, name string, stage string, root string, workspace string, task Task, linkMap map[string]string) bool {
 	containerName := fmt.Sprintf("%s-%s-%s-%s", namespace, name, stage, task.Name)
 	containerName = strings.Replace(containerName, " ", "_", -1)
 
@@ -104,8 +162,6 @@ func executeContainer(namespace string, name string, stage string, root string, 
 		return false
 	}
 
-	fmt.Println("--> Start Container:" + containerName)
-
 	if workspace == "" {
 		workspace = "/workspace"
 	}
@@ -116,7 +172,15 @@ func executeContainer(namespace string, name string, stage string, root string, 
 		strings.Join(task.Commands, "&&"),
 	}
 
-	c, err := cli.ContainerCreate(context.Background(), &container.Config{
+	links := make([]string, len(linkMap))
+
+	for name, container := range linkMap {
+		links = append(links, fmt.Sprintf("%s:%s", container, name))
+	}
+
+	fmt.Println(links)
+
+	cid, err := startContainer(&container.Config{
 		Tty:          true,
 		Image:        task.Box,
 		AttachStdout: true,
@@ -126,6 +190,7 @@ func executeContainer(namespace string, name string, stage string, root string, 
 		Env:          task.Environment,
 	}, &container.HostConfig{
 		AutoRemove: false,
+		// Links:      links,
 		Mounts: []mount.Mount{
 			mount.Mount{
 				Type:   mount.TypeBind,
@@ -133,21 +198,14 @@ func executeContainer(namespace string, name string, stage string, root string, 
 				Target: workspace,
 			},
 		},
-	}, &network.NetworkingConfig{}, containerName)
+	}, containerName)
 
 	if err != nil {
 		log.Fatalln(err)
 		return false
 	}
 
-	err = cli.ContainerStart(context.Background(), c.ID, types.ContainerStartOptions{})
-
-	if err != nil {
-		log.Fatalln(err)
-		return false
-	}
-
-	reader, err := cli.ContainerLogs(context.Background(), c.ID, types.ContainerLogsOptions{
+	reader, err := cli.ContainerLogs(context.Background(), cid, types.ContainerLogsOptions{
 		ShowStdout: true,
 	})
 
@@ -164,12 +222,50 @@ func executeContainer(namespace string, name string, stage string, root string, 
 	}
 
 	fmt.Println(string(b))
+	err = forceRemoveContainer(cid)
 
-	cli.ContainerRemove(context.Background(), c.ID, types.ContainerRemoveOptions{
-		Force: true,
-	})
+	if err != nil {
+		log.Fatalln(err)
+		return false
+	}
 
 	return true
+}
+
+func startContainer(config *container.Config, hostConfig *container.HostConfig, containerName string) (containerID string, err error) {
+	cli, err := client.NewEnvClient()
+	if err != nil {
+		log.Fatalln(err)
+		return
+	}
+
+	c, err := cli.ContainerCreate(context.Background(), config, hostConfig, &network.NetworkingConfig{}, containerName)
+	if err != nil {
+		log.Fatalln(err)
+		return
+	}
+
+	err = cli.ContainerStart(context.Background(), c.ID, types.ContainerStartOptions{})
+
+	if err != nil {
+		log.Fatalln(err)
+		return
+	}
+
+	return c.ID, err
+}
+
+func forceRemoveContainer(cid string) (err error) {
+	cli, err := client.NewEnvClient()
+	if err != nil {
+		log.Fatalln(err)
+		return
+	}
+
+	err = cli.ContainerRemove(context.Background(), cid, types.ContainerRemoveOptions{
+		Force: true,
+	})
+	return
 }
 
 // checkError args[0] force to interrupt programe

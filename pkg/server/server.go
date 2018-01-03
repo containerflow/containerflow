d0549c84c8c5bae17553638ab5deccee4aee1dd1
yunl.zheng <yunl.zheng@gmail.com>
2018-01-03 15:38:21 +0800

    add website

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -1,7 +1,11 @@
 package server
 
 import (
+	"log"
+
+	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
+	"gopkg.in/mgo.v2"
 )
 
 // Server container web server
@@ -14,6 +18,17 @@ type Server struct {
 // Start start web server
 func (s *Server) Start() {
 	r := gin.Default()
+
+	session, err := mgo.Dial("localhost:27017/test")
+
+	if err != nil {
+		log.Fatalln("mongodb connection error", err)
+	}
+
+	c := session.DB("").C("sessions")
+	store := sessions.NewMongoStore(c, 3600, true, []byte("secret"))
+	r.Use(sessions.Sessions("mysession", store))
+
 	r.GET("/ping", s.Health)
 	r.GET("/oauth_redirect", s.GithubOauthRedirect)
 	r.GET("/oauth_callback", s.GithubOAuthCallback)
